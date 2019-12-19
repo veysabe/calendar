d9b4018a1ef7bcef95c752662ee2831f4c043222
veysabe <veysabe@gmail.com>
2019-12-19 14:48:14 +0300

    Added weekend choice

--- js/__calendar-script.js
+++ js/__calendar-script.js
@@ -6,7 +6,69 @@ $(document).ready(function(){
 
 let curDate = new Date();
 
-function createCalendar(elem, year, month, user) {
+let monthName;
+
+let weekendObj = {};
+
+let curMonth;
+
+let month;
+
+function getMonthName(month) {
+	switch (month) {
+	case 1:
+		monthName = 'Январь';
+		break;
+	case 2:
+		monthName = 'Февраль';
+		break;
+	case 3:
+		monthName = 'Март';
+		break;
+	case 4:
+		monthName = 'Апрель';
+		break;
+	case 5:
+		monthName = 'Май';
+		break;
+	case 6:
+		monthName = 'Июнь';
+		break;
+	case 7:
+		monthName = 'Июль';
+		break;
+	case 8:
+		monthName = 'Август';
+		break;
+	case 9:
+		monthName = 'Сентябрь';
+		break;
+	case 10:
+		monthName = 'Октябрь';
+		break;
+	case 11:
+		monthName = 'Ноябрь';
+		break;
+	case 12:
+		monthName = 'Декабрь';
+		break;
+	}; // функция присваивания имени месяца по его номеру
+};
+
+$("body").on('click', '.pickWeekendButton-accept', function(){ // Создание объекта с выходными при нажатии на кнопку
+	let weekendDays = [];
+	$(".slick-slide").each(function(index) {
+		let weekendMonthNum = ($(this).attr('data-month'));
+		$(this).find(".dateCell.toggled").each(function(){
+			weekendDays.push( $(this).text() );
+		})
+		weekendObj[weekendMonthNum] = weekendDays;
+		weekendDays = [];
+	});
+	console.log(weekendObj);
+});
+
+function createCalendar(elem, year, month, user) { // функция создания календаря
 	let mon = month - 1;
 	let d = new Date(year, mon);
 	let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
@@ -33,27 +95,30 @@ function createCalendar(elem, year, month, user) {
 
 	// закрыть таблицу
 	table += '</tr></table>';
-	if (month == 1) monthName = "Январь";
-	if (month == 2) monthName = "Февраль";
-	if (month == 3) monthName = "Март";
-	if (month == 4) monthName = "Апрель";
-	if (month == 5) monthName = "Май";
-	if (month == 6) monthName = "Июнь";
-	if (month == 7) monthName = "Июль";
-	if (month == 8) monthName = "Август";
-	if (month == 9) monthName = "Сентябрь";
-	if (month == 10) monthName = "Октябрь";
-	if (month == 11) monthName = "Ноябрь";
-	if (month == 12) monthName = "Декабрь";
+
+	// if (month == 1) monthName = "Январь";
+	// if (month == 2) monthName = "Февраль";
+	// if (month == 3) monthName = "Март";
+	// if (month == 4) monthName = "Апрель";
+	// if (month == 5) monthName = "Май";
+	// if (month == 6) monthName = "Июнь";
+	// if (month == 7) monthName = "Июль";
+	// if (month == 8) monthName = "Август";
+	// if (month == 9) monthName = "Сентябрь";
+	// if (month == 10) monthName = "Октябрь";
+	// if (month == 11) monthName = "Ноябрь";
+	// if (month == 12) monthName = "Декабрь";
+
+	getMonthName(month);
 
 	content = "<h1>" + monthName + "</h1>" + table;
 	elem.innerHTML = content;
 
 	$(".calendar-slider").slick('refresh');
-
+	$(elem).parent().attr('data-month', month);
 };
 
-$(document).mouseup(function (e){ 
+$(document).mouseup(function (e){ // скрыть модальное окно календаря
 	let calendar = $(".calendar-cont"); 
 	let calendarOut = $(".calendar-out");
 	if (!calendar.is(e.target) // если клик был не по нашему блоку
@@ -61,6 +126,7 @@ $(document).mouseup(function (e){
 			calendarOut.removeClass("shown");
 			calendarOut.removeAttr("style");
 			$(".calendar-cont").removeClass("admin user");
+			$(".pickWeekendButton-accept").remove();
 	}
 });
 
@@ -71,7 +137,7 @@ function getDay(date) {
 	return day - 1;
 };
 
-$(".pickMonthButton").click(function(){
+$(".pickMonthButton").click(function() { // создание календаря при нажатии на кнопку (user)
 	$(".calendar-out").css("display", "flex");
 	$(".calendar-cont").addClass("user");
 	setTimeout(function(){
@@ -79,6 +145,7 @@ $(".pickMonthButton").click(function(){
 	},100);
 	let curYear = curDate.getFullYear();
 	let curMonth = curDate.getMonth() + 1;
+	createCalendar(calendar__current, curYear, curMonth, "user");
 	if (curMonth = 12) {
 		nextMonth = 1;
 		nextYear = curYear + 1;
@@ -86,18 +153,19 @@ $(".pickMonthButton").click(function(){
 		nextMonth = curMonth + 1;
 		nextYear = curYear;
 	}
-	createCalendar(calendar__current, curYear, curMonth, "user");
 	createCalendar(calendar__next, nextYear, nextMonth, "user");
 });
 
-$(".pickWeekendButton").click(function(){
+let adminWeekendButton = '<input type="button" class="pickWeekendButton-accept" value="Выбрать выходные">';
+
+$(".pickWeekendButton").click(function(){ // создание календаря при нажатии на кнопку (user)
 	$(".calendar-out").css("display", "flex");
 	$(".calendar-cont").addClass("admin");
 	setTimeout(function(){
 		$(".calendar-out").addClass("shown");
 	},100);
-	let curYear = curDate.getFullYear();
-	let curMonth = curDate.getMonth() + 1;
+	curYear = curDate.getFullYear();
+	curMonth = curDate.getMonth() + 1;
 	if (curMonth = 12) {
 		nextMonth = 1;
 		nextYear = curYear + 1;
@@ -107,4 +175,13 @@ $(".pickWeekendButton").click(function(){
 	}
 	createCalendar(calendar__current, curYear, curMonth, "admin");
 	createCalendar(calendar__next, nextYear, nextMonth, "admin");
+	$(".calendar-cont").append(adminWeekendButton);
+});
+
+$("body").on('click', '.dateCell.admin', function(){
+	if ( $(this).hasClass('toggled') ) {
+		$(this).removeClass('toggled');
+	} else {
+		$(this).addClass('toggled');
+	}
 });
